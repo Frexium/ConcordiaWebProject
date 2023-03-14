23b5c19087b566d46cd35c6c3e61c1c191ab62eb
Demonis0 <109516237+Demonis0@users.noreply.github.com>
2023-03-14 23:17:47 +0100

    Init Commit

---
+++ views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="co container">
+    <MenuComponent index-active=1></MenuComponent>
+<!--    <div class="extension-box">-->
+<!--      <img class="extension-img" src="../assets/CZ_FR.png">-->
+<!--      <img class="extension-img" src="../assets/CRE_FR.png">-->
+<!--    </div>-->
+<!--    <DisplayerComponent numbers="233"></DisplayerComponent>-->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+import MenuComponent from "@/components/Menu.vue";
+
+export default {
+  name: 'HomeView',
+  components: {MenuComponent},
+  data () {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style scoped>
+.co {
+  display: flex;
+  flex-direction: column;
+}
+.extension-box {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 10%;
+}
+.extension-img {
+  width: 30%;
+  height: 10%;
+}
+</style>
