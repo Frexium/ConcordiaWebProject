b6f5171f0de725bc7a346665d372b78941853d3d
LeXA7513 <autogue.axel@gmail.com>
2023-03-15 00:19:33 -0400

    About Us remplie
    Demandera de changer l'image team_project

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,21 @@
   <div class="co container">
     <MenuComponent index-active=4></MenuComponent>
     <DisplayerComponent :numbers=230></DisplayerComponent>
+    <div >
+      <br>
+          <h1 class="header_type">The Team Project</h1>
+          <br>
+          <img class=img-responsive id=team_project src="../assets/team_project.gif">
+          <br><br>
+          <p class="header_type">
+            We are a team of French students in engineering school and we are passionate about trading cards and especially Pokémons.
+            So through this project we wanted to share our passion. A lot of Pokédex exist online and we also found websites where all the cards are listed. However, we did not propose to make exchanges between collectors or to mark which card we had.
+          </p>
+          <h2 class="header_type">That's where the idea came from!</h2>
+          <br>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -14,3 +28,12 @@ export default {
   components: {MenuComponent}
 }
 </script>
+<style scoped>
+.header_type{
+ font-family: Apple Chancery, cursive ; 
+}
+#team_project{
+  border: solid;
+  border-radius: 50% 20% / 10% 40%;
+}
+</style>
