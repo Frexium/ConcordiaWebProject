fb3f8dede8aec465fd807a59c5ded2975486b827
Demonis0 <109516237+Demonis0@users.noreply.github.com>
2023-03-15 03:06:31 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'jquery'
-import 'popper.js'
-
-createApp(App).use(router).mount('#app')
